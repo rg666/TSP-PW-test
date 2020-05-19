ab5e89ab848a58868e989ad04b2a6523104475cd
Ranadeep Ghosh <ranadeep.mailbox@gmail.com>
2020-05-19 11:07:49 +0200

    Update run.sh
    updated the run port number to 8001.

--- tsp_plotWise_backend/run.sh
+++ tsp_plotWise_backend/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --network myapp_net --name tsp_plotwise_backend repo/tsp-plotwise-backend:1.0.0 /bin/bash
+docker run -d --network myapp_net --name tsp_plotwise_backend -p 8001:8001 repo/tsp-plotwise-backend:1.0.0 /bin/bash
